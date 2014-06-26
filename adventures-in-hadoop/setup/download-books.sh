8751807bba202f4c260143c3e1d48dad9d0aa945
Chris Albright <calbright@cj.com>
2014-06-25 17:13:16 -0700

    Cleaning up; adding more books

--- adventures-in-hadoop/setup/download-books.sh
+++ adventures-in-hadoop/setup/download-books.sh
@@ -7,6 +7,6 @@ cat ${bookIdsFile} | while read id title
 do
     echo Downloading ${title}
     titleString=$(echo ${title} | sed 's/\s/-/g')
-    curl -o ${titleString}.txt http://www.gutenberg.org/files/${id}/${id}.txt
+    curl --user-agent "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36" -o ${titleString}.txt http://www.gutenberg.org/files/${id}/${id}.txt
     sleep $(( $RANDOM % 55 + 5 ))
 done
--- adventures-in-hadoop/setup/setup-filesystem.sh
+++ adventures-in-hadoop/setup/setup-filesystem.sh
@@ -9,9 +9,6 @@ hadoop fs -rm -r /tmp/adventures-in-hadoop
 
 hadoop fs -mkdir -p \
 	/tmp/adventures-in-hadoop/input \
-	/tmp/adventures-in-hadoop/map-reduce/inverted-index/output \
-	/tmp/adventures-in-hadoop/map-reduce/tf-idf/output \
-	/tmp/adventures-in-hadoop/crunch/inverted-index/output \
-	/tmp/adventures-in-hadoop/crunch/tf-idf/output
+	/tmp/adventures-in-hadoop/map-reduce/inverted-index/output
 
 hadoop fs -put $SCRIPT_DIR/data/* /tmp/adventures-in-hadoop/input
